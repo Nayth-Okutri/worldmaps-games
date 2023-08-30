79c83bb6ea41611deef895349773239bbbdb33a6
Nayth <nayth.sphere@gmail.com>
2023-08-30 13:16:51 +0200

    Factorize and improve level displays for home and ranking

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -2,4 +2,4 @@ export const GAME_MODE_DUPLICATE = 1;
 export const GAME_MODE_10_QUESTS = 2;
 export const GAME_MODE_TIMEATTACK = 3;
 export const GAME_MODE_ALLQUESTS = 4;
-export const TIMEATTACK_TIME = 30;
+export const TIMEATTACK_TIME = 20;
