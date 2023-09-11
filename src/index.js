02fb260bbc5a4b870f2955ffe03e9e8b17c29a71
Nayth <nayth.sphere@gmail.com>
2023-09-11 22:17:43 +0200

    remove console logs from prod env

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import App from "./App";
 import { AuthProvider } from "./auth";
 import { UserProvider } from "./userContext"; // Import the UserProvider from your context file
 
+if (process.env.NODE_ENV === "production") {
+  console.log = () => {};
+  console.error = () => {};
+  console.debug = () => {};
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
