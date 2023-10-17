b022251c6d83eef827865af77181c91f12339c48
Nayth <nayth.sphere@gmail.com>
2023-10-17 08:32:49 +0200

    tirage au sort amélioré

--- src/components/PopupResults.js
+++ src/components/PopupResults.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
-
+import "../styles/PopupForm.css";
 const PopupRepliesTable = () => {
   const [replies, setReplies] = useState([]);
   const [result, setResult] = useState("");
@@ -22,7 +22,18 @@ const PopupRepliesTable = () => {
       const db = getFirestore();
       const repliesCollectionRef = collection(db, "popupReplies");
       const repliesSnapshot = await getDocs(repliesCollectionRef);
-      const repliesData = repliesSnapshot.docs.map((doc) => doc.data());
+      const repliesData = repliesSnapshot.docs.map((doc) => {
+        const data = doc.data();
+        let [username, domain] = data.email.split("@");
+
+        // Remplace les trois derniers caractères du nom d'utilisateur par des étoiles
+        let maskedUsername = username.slice(0, -3) + "***";
+
+        // Reconstitue l'adresse e-mail
+        let maskedEmail = maskedUsername + "@" + domain;
+        data.maskedEmail = maskedEmail;
+        return { ...data, vote: 0 };
+      });
       setReplies(repliesData);
     };
 
@@ -33,6 +44,12 @@ const PopupRepliesTable = () => {
   }
   const getOneResult = () => {
     const randomIndex = getRandomIndex(replies);
+    const newrepliesData = replies;
+    if (newrepliesData[randomIndex].vote === 1) {
+      alert(`Gagnant !! ${newrepliesData[randomIndex].email}`);
+    }
+    newrepliesData[randomIndex].vote += 1;
+    setReplies(newrepliesData);
     setResult(replies[randomIndex]);
   };
   return (
@@ -45,19 +62,37 @@ const PopupRepliesTable = () => {
         alignItems: "center",
       }}
     >
+      <h2>Nombre de participants {replies.length}</h2>
+      <button onClick={getOneResult}>Tirer au sort</button>
       <table>
         <thead>
           <tr>
             <th>MAIL</th>
             <th>DATE</th>
+            <th>POINTS</th>
+            <th>NEWSLETTER</th>
             {/* Add more table headers as needed */}
           </tr>
         </thead>
         <tbody>
           {replies.map((reply, index) => (
             <tr key={index}>
-              <td>{reply.email}</td>
+              <td>{reply.maskedEmail}</td>
               <td>{getDateFromFBTimestamp(reply.date)}</td>
+              <td>
+                {" "}
+                <div className="vote-icon">
+                  {[...Array(reply.vote)].map((_, i) => (
+                    <span
+                      key={i}
+                      className={reply.vote === 2 ? "yellow-dot" : "green-dot"}
+                    ></span>
+                  ))}
+                </div>
+              </td>
+              <td>
+                {reply.newsletterChecked ? "Inscrit à la newsletter" : ""}
+              </td>
               {/* Add more table data cells as needed */}
             </tr>
           ))}
@@ -71,7 +106,6 @@ const PopupRepliesTable = () => {
           {result.email}
         </p>
       )}
-      <button onClick={getOneResult}>Tirer au sort</button>
     </div>
   );
 };
